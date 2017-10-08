7a31e641d0a3479d344e61f96f57a164791431f2
Adrian Irving-Beer <wisq@wisq.net>
2017-10-08 16:49:36 -0400

    Suppress compile warnings re: Briefly.

--- lib/mix/task/git.test.ex
+++ lib/mix/task/git.test.ex
@@ -41,10 +41,14 @@ defmodule Mix.Tasks.Git.Test do
     IO.puts([IO.ANSI.light_green, "* ", text, IO.ANSI.reset])
   end
 
-  defp create_tmpdir do
-    {:ok, _started} = Application.ensure_all_started(:briefly)
-    {:ok, tmpdir} = Briefly.create(directory: true)
-    tmpdir
+  if Mix.env == :test do
+    defp create_tmpdir do
+      {:ok, _started} = Application.ensure_all_started(:briefly)
+      {:ok, tmpdir} = Briefly.create(directory: true)
+      tmpdir
+    end
+  else
+    defp create_tmpdir, do: raise "not implemented"
   end
 
   defp symlink(item, from, to) do
